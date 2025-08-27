e6ff95fc87881a924bc1e672194bfc5a9e6ab100
alaanabil45 <alaanab2005@gmail.com>
2025-08-27 17:10:53 +0200

    blogs page completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
+import { Blogs } from './features/blogs/blogs';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
   { path: 'home', loadComponent: () => import('./features/home/home/home').then(m => m.Home) },
   { path: 'auth/login', loadComponent: () => import('./features/auth/login/login').then(m => m.Login) },
   { path: 'auth/register', loadComponent: () => import('./features/auth/register/register').then(m => m.Register) },
+  { path: 'blogs', component: Blogs },
+  { path: '', redirectTo: 'blogs', pathMatch: 'full' },
   
   {
   path: 'product/:id',
